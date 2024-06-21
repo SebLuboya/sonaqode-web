6f03007234514bd540f8369b47d982714f31bb98
CozyBrian <dragonbrian33@gmail.com>
2024-06-21 04:27:45 +0000

    add: images for corporate

--- src/components/about/faq.tsx
+++ src/components/about/faq.tsx
@@ -1,6 +1,33 @@
+"use client";
 import MinusCircle from "@/assets/icons/glyphs/MinusCircle";
 import PlusCircle from "@/assets/icons/glyphs/PlusCircle";
-import React from "react";
+import { cn } from "@/utils";
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+
+const data = [
+  {
+    question: "When can we start our work?",
+    answers:
+      "There is no need for delay! Take action immediately. Browse and subscribe or send us your request, and we will promptly get in touch with you.",
+  },
+  {
+    question: "How will I track the progress of my project?",
+    answers: "",
+  },
+  {
+    question: "Where is SonaQode based?",
+    answers: "",
+  },
+  {
+    question: "How do you safeguard my intellectual property?",
+    answers: "",
+  },
+  {
+    question: "Can you tell me more about your hiring process?",
+    answers: "",
+  },
+];
 
 const Faq = () => {
   return (
@@ -11,31 +38,52 @@ const Faq = () => {
             Frequently Asked Questions
           </h2>
           <div className="w-full flex flex-col gap-4">
-            <div className="flex flex-row gap-5 lg:gap-6 bg-white p-6 lg:p-8 rounded-[30px]">
-              <MinusCircle className="w-5 h-5 shrink-0 text-[#98A2B3] mt-1" />
-              <div className="flex flex-col gap-1.5 lg:gap-2">
-                <p className="font-medium text-base lg:text-lg">
-                  When can we start our work?
-                </p>
-                <p className="text-sm lg:text-base font-light">
-                  There is no need for delay! Take action immediately. Browse
-                  and subscribe or send us your request, and we will promptly
-                  get in touch with you.
-                </p>
-              </div>
-            </div>
-            <div className="flex flex-row gap-6  p-8 rounded-[30px]">
-              <PlusCircle className="w-5 h-5 shrink-0 text-[#98A2B3] mt-1" />
-              <div className="flex flex-col gap-1.5 lg:gap-2">
-                <p className="font-medium text-base lg:text-lg">
-                  How will I track the progress of my project?
-                </p>
-              </div>
-            </div>
+            {data.map((item) => (
+              <FaqItem key={item.question} {...item} />
+            ))}
           </div>
         </div>
       </div>
     </section>
+  );
+};
+
+const FaqItem = ({
+  question = "When can we start our work?",
+  answer = `There is no need for delay! Take action immediately. Browse and
+            subscribe or send us your request, and we will promptly get in touch
+            with you.`,
+}) => {
+  const [isOpen, setisOpen] = useState(false);
+
+  return (
+    <div
+      onClick={() => setisOpen((prev) => !prev)}
+      className={cn(
+        "flex flex-row items-start gap-5 lg:gap-6 p-6 lg:p-8 rounded-[30px] cursor-pointer duration-150",
+        isOpen && "bg-white",
+      )}
+    >
+      <button onClick={() => setisOpen((prev) => !prev)}>
+        {isOpen ? (
+          <MinusCircle className="w-5 h-5 shrink-0 text-[#98A2B3] mt-1" />
+        ) : (
+          <PlusCircle className="w-5 h-5 shrink-0 text-[#98A2B3] mt-1" />
+        )}
+      </button>
+      <div className="flex flex-col items-start">
+        <p className="font-medium text-base lg:text-lg">{question}</p>
+        <motion.div
+          initial={{ height: 0 }}
+          animate={{ height: isOpen ? "auto" : 0 }}
+          className="overflow-clip"
+        >
+          <p className="text-sm lg:text-base font-light pt-1.5 lg:pt-2">
+            {answer}
+          </p>
+        </motion.div>
+      </div>
+    </div>
   );
 };
 
