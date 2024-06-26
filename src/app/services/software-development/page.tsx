410543d1315c5a591f53e0afc23e4b94b76b4fc9
CozyBrian <dragonbrian33@gmail.com>
2024-06-26 01:45:46 +0000

    add: contact and quote pages

--- src/app/services/software-development/page.tsx
+++ src/app/services/software-development/page.tsx
@@ -4,6 +4,15 @@ export const metadata = {
   title: "Software Development – SonaQode",
 };
 
+const data = [
+  {
+    icon: null,
+    title: "",
+    full_time: 2003,
+    part_time: 1000,
+    education: "...",
+  },
+];
 export default function SoftwareDevelopment() {
   return <div>SoftwareDevelopment</div>;
 }
