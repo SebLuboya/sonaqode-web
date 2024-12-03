a31e50afefe7e3d9727289d92c55d3f8c4d796f0
CozyBrian <dragonbrian33@gmail.com>
2024-12-03 20:58:56 +0000

    minor changes

--- src/components/home/clientData.ts
+++ src/components/home/clientData.ts
@@ -6,7 +6,7 @@ export const clientData = [
     position: "CEO, Performr Group ltd",
     review:
       "I approached Sonaqode with a tight timeline to develop version 1 of our MVP. From the outset of the onboarding process, I was immediately impressed by their professionalism and thoroughness. They selected the most suitable candidates who demonstrated a clear understanding of our requirements and delivered on them effectively, maintaining excellent communication throughout. We were thrilled with the quality of the product delivered and are now collaborating on version 2.",
-    country: "UK",
+    country: "UK 🇬🇧",
     image: Performr,
   },
 
@@ -15,7 +15,7 @@ export const clientData = [
     position: "CEO, Syka Bank",
     review:
       "Working with Sonaqode to build Phase 1 of Syka's web application has been a great experience. The initiation process was seamless and comprehensive, demonstrating their commitment to quality. Their highly skilled and efficient developers delivered a product that aligned perfectly with our vision and expectations. We are very satisfied with the product and look forward to more engagements.",
-    country: "GHANA",
+    country: "GHANA 🇬🇭",
     image: Syka,
   },
   {
@@ -23,7 +23,7 @@ export const clientData = [
     position: "Tech lead, My Tech Partner",
     review:
       "Connecting with them has been a joy. We've developed a fruitful collaboration and consider them part of our team. They are very hands-on and actively involved in conversations. They have provided us with highly qualified back-end engineers. Despite our demanding requirements, they remained adaptable and delivered without complaint, consistently meeting our expectations. It’s a pleasure working with Sonaqode. I would highly recommend them.",
-    country: "NETHERLANDS",
+    country: "NETHERLANDS 🇳🇱",
     image: Mytech,
   },
   {
@@ -31,7 +31,7 @@ export const clientData = [
     position: "Director, Afrimedia Ltd",
     review:
       "We contacted Sonaqode because we needed a video editor to complete a project. They were very accommodating in understanding our business, goals, and roadmap. They kept things simple yet concise, which made me feel confident in the service I was acquiring. I commend their excellent communication. The editor assigned to us, Romeo, was a pleasure to work with. He had a proactive attitude and great enthusiasm.",
-    country: "UK",
+    country: "UK 🇬🇧",
     image: Afrimedia,
   },
 ];
